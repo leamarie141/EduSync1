b15c7dd98c2284c123ecb071c23a8332ba5b0b84
ingrid <leamariegozarin@gmail.com>
2026-04-05 15:36:31 +0800

    added a new feature

--- backend/server.js
+++ backend/server.js
@@ -8,8 +8,6 @@ const cors = require("cors");
 const mongoose = require("mongoose");
 
 mongoose.connect(process.env.MONGO_URI, {
-  useNewUrlParser: true,
-  useUnifiedTopology: true,
 }).then(() => console.log("MongoDB connected"))
   .catch(err => console.error("Mongo DB connection error:", err));
 
